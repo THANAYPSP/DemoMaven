1a3723ff283588b26bb6e79340b5c3601c45f87f
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-18 15:21:21 +0700

    add Random7

--- hello-spring/src/test/java/com/example/hellospring/workshop01/GenrtateUUIDTest.java
+++ hello-spring/src/test/java/com/example/hellospring/workshop01/GenrtateUUIDTest.java
@@ -2,13 +2,25 @@ package com.example.hellospring.workshop01;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+class Random7 extends Random{
+    @Override
+    public int nextInt(int bound) {
+        return 7;
+    }
+}
+
 class GenrtateUUIDTest {
+
+
 
     @Test
     public void  getUUID(){
         GenrtateUUID genrtateUUID = new GenrtateUUID();
+        genrtateUUID.setRandom(new Random7());
         String uuid = genrtateUUID.get("Thana");
         assertEquals("XYZThana7",uuid);
     }
