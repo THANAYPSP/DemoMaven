c8b938449a6b7b05c566051e6ac3ce89d04cb94c
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-18 17:20:32 +0700

    Add FixxBuzzV2 (Refactor) and Test

---
+++ hello-spring/src/main/java/com/example/hellospring/workshop01/AllCondition.java
@@ -0,0 +1,61 @@
+package com.example.hellospring.workshop01;
+
+interface MyCondition {
+    boolean Check(Integer input);
+    String Print();
+}
+
+class BuzzCond implements MyCondition {
+    @Override
+    public boolean Check(Integer input) {
+        return input % 5 == 0;
+    }
+
+    @Override
+    public String Print() {
+        return "Buzz";
+    }
+
+}
+class FizzCond implements MyCondition {
+    @Override
+    public boolean Check(Integer input) {
+        return input % 3 == 0;
+    }
+
+    @Override
+    public String Print() {
+        return "Fizz";
+    }
+}
+
+class FizzBuzzCond implements MyCondition {
+    FizzCond fizzCond = new FizzCond();
+    BuzzCond buzzCond = new BuzzCond();
+
+    @Override
+    public boolean Check(Integer input) {
+        if(fizzCond.Check(input) && buzzCond.Check(input)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String Print() {
+        return "FizzBuzz";
+    }
+}
+
+class KBTGCond implements MyCondition {
+
+    @Override
+    public boolean Check(Integer input) {
+        return input%7==0;
+    }
+
+    @Override
+    public String Print() {
+        return "KBTG Java Basic";
+    }
+}
