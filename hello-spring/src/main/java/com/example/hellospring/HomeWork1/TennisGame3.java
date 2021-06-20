d915c018b5cf9a1279f5f98c3c1f7f9c56ae1a46
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-21 00:21:24 +0700

    Refactor First time [carelessly]

--- hello-spring/src/main/java/com/example/hellospring/HomeWork1/TennisGame3.java
+++ hello-spring/src/main/java/com/example/hellospring/HomeWork1/TennisGame3.java
@@ -6,6 +6,7 @@ public class TennisGame3 {
     private int player2Score;
     private String player1Name;
     private String player2Name;
+    String[] point = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
     public TennisGame3(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -14,15 +15,13 @@ public class TennisGame3 {
 
     public String getScore() {
         String score;
-        if (player1Score < 4 && player2Score < 4 && !(player1Score + player2Score == 6)) {
-            String[] point = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        if (nowIsCallDeuce()) {
             score = point[player1Score];
             return (player1Score == player2Score) ? score + "-All" : score + "-" + point[player2Score];
         } else {
-            if (player1Score == player2Score)
+            if (isDeuce())
                 return "Deuce";
-            score = player1Score > player2Score ? player1Name : player2Name;
-            return ((player1Score-player2Score)*(player1Score-player2Score) == 1) ? "Advantage " + score : "Win for " + score;
+            return ((player1Score-player2Score)*(player1Score-player2Score) == 1) ? "Advantage " + getCallName() : "Win for " + getCallName();
         }
     }
 
@@ -36,8 +35,17 @@ public class TennisGame3 {
     }
 
     public String getCallName(){
-        //
         return player1Score > player2Score ? player1Name : player2Name;
     }
+
+    public boolean isDeuce(){
+            return player1Score == player2Score;
+    }
+
+    public boolean nowIsCallDeuce(){
+        return player1Score < 4 && player2Score < 4 && !(player1Score + player2Score == 6);
+    }
+
+
 
 }
