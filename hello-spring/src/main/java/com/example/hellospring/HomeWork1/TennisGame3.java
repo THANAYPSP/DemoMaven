96a8190cd8cfc2399ebcd070d6569d7c83068f1e
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-21 00:04:21 +0700

    Rename variable

--- hello-spring/src/main/java/com/example/hellospring/HomeWork1/TennisGame3.java
+++ hello-spring/src/main/java/com/example/hellospring/HomeWork1/TennisGame3.java
@@ -2,37 +2,42 @@ package com.example.hellospring.HomeWork1;
 
 public class TennisGame3 {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int player1Score;
+    private int player2Score;
+    private String player1Name;
+    private String player2Name;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        String score;
+        if (player1Score < 4 && player2Score < 4 && !(player1Score + player2Score == 6)) {
+            String[] point = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+            score = point[player1Score];
+            return (player1Score == player2Score) ? score + "-All" : score + "-" + point[player2Score];
         } else {
-            if (p1 == p2)
+            if (player1Score == player2Score)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            score = player1Score > player2Score ? player1Name : player2Name;
+            return ((player1Score-player2Score)*(player1Score-player2Score) == 1) ? "Advantage " + score : "Win for " + score;
         }
     }
 
     public void wonPoint(String playerName) {
         if ("player1".equals(playerName)) {
-            this.p1 += 1;
+            this.player1Score += 1;
         } else {
-            this.p2 += 1;
+            this.player2Score += 1;
         }
 
+    }
+
+    public String getCallName(){
+        //
+        return player1Score > player2Score ? player1Name : player2Name;
     }
 
 }
